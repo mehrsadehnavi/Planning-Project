d5b11c414c659251f8aaa0d602964bdf7f92f522
sobhan0011 <sobhanshams7911@gmail.com>
2022-12-30 19:47:15 +0330

    Regression is complete now!
    regress() ,is_unified() and is_conflicting() functions are completed.

--- Action.py
+++ Action.py
@@ -7,7 +7,13 @@ class Action:
         self.delete_list = delete_list
 
     def regress(self, state):
-        state - add_list + positive_preconditions
+        for positive_literal in state.positive_literals:
+            if positive_literal in self.add_list:
+                state.positive_literals.remove(positive_literal)
+        for negative_literal in state.negative_literals:
+            if negative_literal in self.add_list:
+                state.negative_literals.remove(negative_literal)
+        #state - add_list + positive_preconditions
         return
 
     def is_relevant(self, state):
@@ -20,15 +26,13 @@ class Action:
         return True
 
     def is_unified(self, state):
-    #ایا با این اکشن میتوینم بهش برسیم یا نه
-    # add list should be in positive and delete list in negative
-        #write your code
-        return
+        # ایا با این اکشن میتوینم بهش برسیم یا نه
+        # add list should be in positive and delete list in negative
+        return not (set(state.positive_literals).isdisjoint(self.add_list) and set(state.negative_literals).isdisjoint(self.add_list))
 
     def is_conflicting(self, state):
-        #پازتیو اینو با دیلیت لیست(نگاتیو) اون چک میکنه
-        #write your code
-        return
+        # پازتیو اینو با دیلیت لیست(نگاتیو) اون چک میکنه
+        return not (set(state.positive_literals).isdisjoint(self.delete_list) and set(state.negative_literals).isdisjoint(self.delete_list))
 
     def to_string(self):
         return f'action, name: {self.name}, positive preconditions: {self.positive_preconditions}, negative preconditions: {self.negative_preconditions}, add list: {self.add_list}, delete list: {self.delete_list}'
