8d22cc3d47ea52e225d99f9dfc7f8a8f95046b93
Mehran Seyfi <mehran.seyfi@mail.um.ac.ir>
2023-02-04 02:53:27 +0330

    Now blocks world returns results and is checked

--- BlockWorldProblem.py
+++ BlockWorldProblem.py
@@ -1,10 +1,10 @@
 from Action import Action
 from State import State
 
-initial_state = State(None, None, positive_literals=["onAtable", "onCA", "onBtable", "clearB", "clearC", "cleartable"],
-                      negative_literals=["clearA", "onBA", "onBC", "onAB", "onAC", "onAB", "onCB"])
+initial_state = State(None, None, positive_literals=["onAtable", "onCA", "onBtable", "clearB", "clearC", "cleartable", "blockB", "blockC","blockA"],
+                      negative_literals=['onAB', 'onAC', 'onBC', 'onCA', "clearB", "clearC"])
 
-goal_state = State(None, None, positive_literals=["onAB", "onBC", "clearA"],
+goal_state = State(None, None, positive_literals=["onAB", "onBC"],
                    negative_literals=['onAB', 'onAC', 'onBC', 'onCA', "clearB", "clearC"])
 
 def get_actions():
@@ -19,20 +19,29 @@ def get_actions():
                 #     neg_precond = ["on" + block + location, "clear" + block, "clear" + location2]
                 if block != location and block != location2 and location != location2:
                     move_action = Action(name="move" + block + location + location2,
-                                         positive_preconditions=["on" + block + location, "clear" + block,
-                                                                 "clear" + location2],
+                                         positive_preconditions=["on" + block + location,
+                                                                 "clear" + block,
+                                                                 "clear" + location2,
+                                                                 "block" + block,
+                                                                 "block" + location2],
                                          # negative_preconditions= neg_precon,
                                          negative_preconditions=[],
-                                         add_list=["on" + block + location2],
-                                         delete_list=["clear" + location2, "on" + block + location])
-                actions.append(put_action)
+                                         add_list=["on" + block + location2,
+                                                   "clear" + location],
+                                         delete_list=["clear" + location2,
+                                                      "on" + block + location])
+                    actions.append(move_action)
 
     for block in blocks:
         for location in locations:
             move_to_table_action = Action(name="moveToTable" + block + location,
-                                          positive_preconditions=["on" + block + location, "clear" + block],
+                                          positive_preconditions=["on" + block + location,
+                                                                  "clear" + block,
+                                                                  "block" + block,
+                                                                  "block" + location],
                                           negative_preconditions=[],
-                                          add_list=["on" + block + "table", "clear" + block],
+                                          add_list=["on" + block + "table",
+                                                    "clear" + block],
                                           delete_list=["on" + block + location])
             actions.append(move_to_table_action)
 
--- Test.py
+++ Test.py
@@ -1,28 +1,39 @@
 from State import State
 from BackwardSearch import backward_search
 from ForwardSearch import forward_search
-from tireproblem import get_actions
+# from tireproblem import get_actions
+from BlockWorldProblem import get_actions
 import time
 
 
 def main():
     print("Getting the set of all actions...")
     actions = get_actions()
+    # print(actions)
 
     print("Planning...")
-    initial_state = State(None, None, positive_literals=["atflataxle", "atsparetrunk"], negative_literals=['atflatground', 'atflattrunk', 'atspareground', 'atspareaxle'])
-    goal_state = State(None, None, positive_literals=["atspareaxle", 'atflatground'], negative_literals=['atspareground', 'atsparetrunk', 'atflataxle', 'atflattrunk'])
-    # actions = [Action("Generic", positive_preconditions=["A", "B"], negative_preconditions=[], add_list=["C"], delete_list=[]), \
+
+    # initial_state = State(None, None, positive_literals=["atflataxle", "atsparetrunk"],
+    #                       negative_literals=['atflatground', 'atflattrunk', 'atspareground', 'atspareaxle'])
+    # goal_state = State(None, None, positive_literals=["atspareaxle", 'atflatground'],
+    #                    negative_literals=['atspareground', 'atsparetrunk', 'atflataxle', 'atflattrunk'])
+
+    initial_state = State(None, None,
+                          positive_literals=["onAtable", "onCA", "onBtable", "clearB", "clearC", "cleartable", "blockB",
+                                             "blockC", "blockA"],
+                          negative_literals=["clearA", "onBA", "onBC", "onAB", "onAC", "onAB", "onCB"])
+
+    goal_state = State(None, None, positive_literals=["onAB", "onBC"],
+                       negative_literals=['onAB', 'onAC', 'onBC', 'onCA', "clearB", "clearC"])
     # Action("Generic", positive_preconditions=["B", "C"], negative_preconditions=[], add_list=["D"], delete_list=[])]
-    # backward_search(goal_state, initial_state, actions)
     start_time = time.time()
     print('Backward Search Answer:')
     backward_search(goal_state, initial_state, actions, True)
     print("--- %s seconds ---" % (time.time() - start_time))
-    start_time = time.time()
-    print('Forward Search Answer:')
-    forward_search(goal_state, initial_state, actions, True)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    # start_time = time.time()
+    # print('Forward Search Answer:')
+    # forward_search(goal_state, initial_state, actions, True)
+    # print("--- %s seconds ---" % (time.time() - start_time))
 
 
 if __name__ == "__main__":
