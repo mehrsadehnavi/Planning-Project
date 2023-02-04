ee9165275e70ea896c590779327c987071741b59
Mehran Seyfi <mehran.seyfi@mail.um.ac.ir>
2023-02-04 16:42:00 +0330

    Completed

--- BlockWorldProblem.py
+++ BlockWorldProblem.py
@@ -1,11 +1,19 @@
 from Action import Action
 from State import State
 
-initial_state = State(None, None, positive_literals=["onAtable", "onCA", "onBtable", "clearB", "clearC", "cleartable", "blockB", "blockC","blockA"],
-                      negative_literals=['onAB', 'onAC', 'onBC', 'onCA', "clearB", "clearC"])
+initial_state = State(None, None,
+                      positive_literals=["onAtable", "onCA", "onBtable",
+                                         "clearB", "clearC", "cleartable",
+                                         "blockB", "blockC", "blockA"],
+                      negative_literals=['onAB', 'onAC', 'onBC',
+                                         'onCA', "clearB", "clearC"]
+                      )
 
-goal_state = State(None, None, positive_literals=["onAB", "onBC"],
-                   negative_literals=['onAB', 'onAC', 'onBC', 'onCA', "clearB", "clearC"])
+goal_state = State(None, None,
+                   positive_literals=["onAB", "onBC"],
+                   negative_literals=['onAB', 'onAC', 'onBC',
+                                      'onCA', "clearB", "clearC"]
+                   )
 
 def get_actions():
     actions = []
@@ -15,8 +23,8 @@ def get_actions():
     for block in blocks:
         for location in locations:
             for location2 in locations:
-                # if block == location and block == location2 and location == location2:
-                #     neg_precond = ["on" + block + location, "clear" + block, "clear" + location2]
+                if block == location and block == location2 and location == location2:
+                    neg_precond = ["on" + block + location, "clear" + block, "clear" + location2]
                 if block != location and block != location2 and location != location2:
                     move_action = Action(name="move" + block + location + location2,
                                          positive_preconditions=["on" + block + location,
