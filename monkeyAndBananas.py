50e6ad09f52953eed4b12b9a71925007d4bec8e3
Mehran Seyfi <mehran.seyfi@mail.um.ac.ir>
2023-02-04 16:41:07 +0330

    Completed - No test

--- monkeyAndBananas.py
+++ monkeyAndBananas.py
@@ -1,77 +1,92 @@
 from Action import Action
 from State import State
 
-initial_state = State(None, None, positive_literals=["atmonkeyA", "atbananasB", "atboxC", "hightmonkeylow",
-                                                     "heightboxlow", "heigthbananashigh","pushablebox",
-                                                     "climbablebox", "graspablebananas"],
-                      negative_literals=["equalAB", "equalAC", "equalBA",
-                                         "equalBC", "equalCA", "equalCB"])
+initial_state = State(None, None,
+                      positive_literals=["atmonkeyA",
+                                         "atbananasB",
+                                         "atboxC",
+                                         "hightmonkeylow",
+                                         "heightboxlow",
+                                         "heigthbananashigh",
+                                         "pushablebox",
+                                         "climbablebox",
+                                         "graspablebananas"],
 
-goal_state = State(None, None, positive_literals=["havemonkeybananas"],
-                   negative_literals=[])
+                      negative_literals=["equalAB",
+                                         "equalAC",
+                                         "equalBA",
+                                         "equalBC",
+                                         "equalCA",
+                                         "equalCB"]
+                      )
+
+goal_state = State(None, None,
+                   positive_literals=["havemonkeybananas"],
+                   negative_literals=[]
+                   )
 
 def get_actions():
     actions = []
     locations = ["x", "y"]
     heights = ["low", "high"]
-    bs = []
+    objects = ["monkey", "benanas"]
 
     for loc in locations:
         for loc2 in locations:
             if loc != loc2:
                 go_action = Action(name="go" + loc + loc2,
-                             positive_preconditions=["at" + "monkey" + loc,
+                             positive_preconditions = ["at" + "monkey" + loc,
                                                      "hight" + "monkey" + "low"],
-                             negative_preconditions=[],
-                             add_list=["at" + "monkey" + loc2],
-                             delete_list=["at" + "monkey", + loc])
+                             negative_preconditions = [],
+                             add_list = ["at" + "monkey" + loc2],
+                             delete_list = ["at" + "monkey", + loc])
             actions.append(put_action)
 
     for loc in locations:
         for loc2 in locations:
-            for b in bs:
+            for obj in objects:
                 if loc != loc2:
-                    push_action = Action(name="push" + b + loc + loc2,
-                                                  positive_preconditions=["at" + "monkey" + loc,
+                    push_action = Action(name = "push" + obj + loc + loc2,
+                                                  positive_preconditions = ["at" + "monkey" + loc,
                                                                           "height" + "monkey" + "low",
-                                                                          "at" + b + loc,
-                                                                          "pushable" + b,
-                                                                          "height" + b + "low"],
-                                                  negative_preconditions=["equal"+ loc + loc2],
-                                                  add_list=["at" + "monkey" + loc,
+                                                                          "at" + obj + loc,
+                                                                          "pushable" + obj,
+                                                                          "height" + obj + "low"],
+                                                  negative_preconditions = ["equal"+ loc + loc2],
+                                                  add_list = ["at" + "monkey" + loc,
                                                             "at" + "monkey" + loc2],
-                                                  delete_list=["at" + b + loc,
+                                                  delete_list = ["at" + obj + loc,
                                                                "at" + "monkey" + loc])
                 actions.append(move_to_table_action)
 
     for loc in locations:
-        for b in bs:
-            climb_action = Action(name="climb" + loc + b,
+        for obj in objects:
+            climb_action = Action(name="climb" + loc + obj,
                                  positive_preconditions=["at" + "monkey" + loc,
                                                          "height" + "monkey" + "low",
-                                                         "at" + b + loc,
-                                                         "climbable" + b,
-                                                         "height" + b + "low"],
+                                                         "at" + obj + loc,
+                                                         "climbable" + obj,
+                                                         "height" + obj + "low"],
                                  negative_preconditions=[],
-                                 add_list=["on" + "monkey" + b,
+                                 add_list=["on" + "monkey" + obj,
                                            "height" + "monkey" + "high"],
                                  delete_list=["height" + "monkey" + "low"])
             actions.append(move_to_table_action)
 
 
     for loc in locations:
-        for b in bs:
+        for obj in objects:
             for height in heights:
-                grasp_action = Action(name = "grasp" + loc + b + height,
+                grasp_action = Action(name = "grasp" + loc + obj + height,
                                               positive_preconditions=["at" + "monkey" + loc,
                                                                       "height" + "monkey" + height,
-                                                                      "at" + b + loc,
-                                                                      "graspable" + b,
-                                                                      "height" + b + height],
+                                                                      "at" + obj + loc,
+                                                                      "graspable" + obj,
+                                                                      "height" + obj + height],
                                               negative_preconditions=[],
-                                              add_list=["have" + "monkey" + b],
-                                              delete_list=["at" + b + loc,
-                                                           "height" + b + height])
+                                              add_list=["have" + "monkey" + obj],
+                                              delete_list=["at" + obj + loc,
+                                                           "height" + obj + height])
                 actions.append(move_to_table_action)
 
 
