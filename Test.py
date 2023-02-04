1f06ec83dc6c5042f62e63d64bc589f6cf7463c3
Mehrsa Dehnavi <86672567+mehrsadehnavi@users.noreply.github.com>
2023-02-04 16:09:30 +0330

    Returning the result of Depot problem.

--- Test.py
+++ Test.py
@@ -2,7 +2,8 @@ from State import State
 from BackwardSearch import backward_search
 from ForwardSearch import forward_search
 # from tireproblem import get_actions
-from BlockWorldProblem import get_actions
+# from BlockWorldProblem import get_actions
+from DepotDomain import get_actions
 import time
 
 
@@ -18,13 +19,22 @@ def main():
     # goal_state = State(None, None, positive_literals=["atspareaxle", 'atflatground'],
     #                    negative_literals=['atspareground', 'atsparetrunk', 'atflataxle', 'atflattrunk'])
 
-    initial_state = State(None, None,
-                          positive_literals=["onAtable", "onCA", "onBtable", "clearB", "clearC", "cleartable", "blockB",
-                                             "blockC", "blockA"],
-                          negative_literals=["clearA", "onBA", "onBC", "onAB", "onAC", "onAB", "onCB"])
+#    initial_state = State(None, None,
+#                          positive_literals=["onAtable", "onCA", "onBtable", "clearB", "clearC", "cleartable", "blockB",
+#                                             "blockC", "blockA"],
+#                          negative_literals=["clearA", "onBA", "onBC", "onAB", "onAC", "onAB", "onCB"])
 
-    goal_state = State(None, None, positive_literals=["onAB", "onBC"],
-                       negative_literals=['onAB', 'onAC', 'onBC', 'onCA', "clearB", "clearC"])
+#   goal_state = State(None, None, positive_literals=["onAB", "onBC"],
+#                       negative_literals=['onAB', 'onAC', 'onBC', 'onCA', "clearB", "clearC"])
+    initial_state = State(None, None, positive_literals=["atP0D0", "clearC0", "atP1D0", "clearC3", "atP2D1", "clearC2",
+                                                         "atT0D0", "atT1D0", "atH0D0", "availableH0", "atH1D0",
+                                                         "availableH1",
+                                                         "atH2D1", "availableH2", "atC0D0", "onC0P0", "atC1D1",
+                                                         "onC1P1",
+                                                         "atC2D1", "onC2C1", "atC3D0", "onC3P1"],
+                          negative_literals=["clearP0", "clearD0", ""])
+    goal_state = State(None, None, positive_literals=["onC0P2", "onC1P3", "onC2P0", "onC3P1"])
+
     # Action("Generic", positive_preconditions=["B", "C"], negative_preconditions=[], add_list=["D"], delete_list=[])]
     start_time = time.time()
     print('Backward Search Answer:')
